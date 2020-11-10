c936d6f4095c9fd4e252a2f739a92741f6fa1af2
Angelo <7332321+antjowie@users.noreply.github.com>
2020-11-10 07:02:18 +0100

    Added a player move script and a avatar mask that doesn't affect arms

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool up = Input.GetKey("w");
+        bool down = Input.GetKey("a");
+        bool left = Input.GetKey("s");
+        bool right = Input.GetKey("d");
+
+        Vector2 move = new Vector2();
+    }
+}
