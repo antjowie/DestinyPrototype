4b4858f724707cff7a69bf883dc741a2dadc18ce
Angelo <7332321+antjowie@users.noreply.github.com>
2020-11-12 16:00:38 +0100

    Added a script to follow the camera with damping

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = Vector3.zero;
+    public float smoothTime = 0.2f;
+    
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        transform.parent = null;
+    }
+    
+    void Update()
+    {
+        // Vector3 delta = target.position - transform.position;
+        Vector3 targetPos = target.TransformPoint(offset);
+        
+        transform.position = Vector3.SmoothDamp(transform.position,targetPos,ref velocity, smoothTime);
+    }
+}
